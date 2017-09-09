1fb8de3c00da68ccf36b003caa8debaea6a50c1c
Stephen Angelico <raaff35lightning@gmail.com>
2017-09-09 11:22:16 +1000

    doorbell: remove rollover and manually sleep instead

--- doorbell.py
+++ doorbell.py
@@ -1,12 +1,14 @@
 # Listens for doorbell signal (GPIO on RPi) and sends request to server
 from gpiozero import Button
 import requests
+import time
 
-button = Button(3, bounce_time=5)
+button = Button(3)
 
 def bell_ring():
 	button.wait_for_press()
 	requests.post('http://f-22raptor:8088/ring')
+	time.sleep(5)
 
 if __name__ == '__main__':
 	bell_ring()
