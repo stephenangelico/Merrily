5283d1f32922442a659f37d2baef3538758025a7
Stephen Angelico <raaff35lightning@gmail.com>
2017-09-09 10:58:52 +1000

    doorbell: use gpiozero.Button.wait_for_press to do all the work

--- doorbell.py
+++ doorbell.py
@@ -1,4 +1,12 @@
 # Listens for doorbell signal (GPIO on RPi) and sends request to server
-#import gpiozero
+from gpiozero import Button
 import requests
 
+button = Button(4, bounce_time=5)
+
+def bell_ring():
+	button.wait_for_press()
+	requests.post('http://f-22raptor:8088/ring')
+
+if __name__ == '__main__':
+	bell_ring()
