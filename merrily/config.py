b185ed54e9aa8ed4fbab574f0c56142a505db11b
Stephen Angelico <raaff35lightning@gmail.com>
2018-04-30 05:31:22 +1000

    config: Make DB username and password more generic

--- merrily/config.py
+++ merrily/config.py
@@ -1,15 +1,15 @@
 import os
 class DeploymentConfig(object):
-	DATABASE_URI = "postgresql://stephen:thinkful@localhost:5432/merrily"
+	DATABASE_URI = "postgresql://username:password@localhost:5432/merrily"
 	DEBUG = False
 	SECRET_KEY = os.environ.get("MERRILY_SECRET_KEY", os.urandom(12))
 
 class DevelopmentConfig(object):
-	DATABASE_URI = "postgresql://stephen:thinkful@localhost:5432/merrily"
+	DATABASE_URI = "postgresql://username:password@localhost:5432/merrily"
 	DEBUG = True
 	SECRET_KEY = os.environ.get("MERRILY_SECRET_KEY", os.urandom(12))
 
 class TestingConfig(object):
-	DATABASE_URI = "postgresql://stephen:thinkful@localhost:5432/merrily-test"
+	DATABASE_URI = "postgresql://username:password@localhost:5432/merrily-test"
 	DEBUG = True
 	SECRET_KEY = os.environ.get("MERRILY_SECRET_KEY", os.urandom(12))
