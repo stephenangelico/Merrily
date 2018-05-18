ae42b5ac42ad02174f464c95f585eaa01f1fc623
Stephen Angelico <raaff35lightning@gmail.com>
2018-05-18 16:39:16 +1000

    Add SystemD installer for log_server.py

---
+++ install_log_server.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Install SystemD service for the hardware doorbell detector
+instl ()
+{ echo "[Unit]
+Description=Merrily Log Server
+After=network.target
+
+[Service]
+Type=simple
+Environment=VIRTUAL_ENV='`pwd`/env'
+User=`echo $SUDO_USER`
+WorkingDirectory=`pwd`
+ExecStart=`pwd`/env/bin/python3 `pwd`/log_server.py
+Restart=always
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+" >/etc/systemd/system/merrily_log.service
+systemctl --system daemon-reload
+	systemctl enable merrily_log.service
+	echo Installed as merrily_log.service.
+	systemctl start merrily_log.service
+}
+
+if [[ `id -u` -ne 0 ]] ; then
+	echo "This installer must be run as root."
+	exit 1
+fi
+read -p "This will install log_server.py as a system service. Continue? [y/n] " -r
+if [[ $REPLY =~ ^[Yy] ]] ; then
+	instl
+else
+	echo "Aborted."
+fi
