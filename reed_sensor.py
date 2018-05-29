cf13c63462e2a2b0c8b232af69981fb31ef79ff6
Stephen Angelico <raaff35lightning@gmail.com>
2018-05-29 22:29:03 +1000

    reed_sensor: Remove separate thread for accepting connections

--- reed_sensor.py
+++ reed_sensor.py
@@ -1,5 +1,5 @@
 # Door sensor for sending a message if a door is opened
-# Reference design uses a Reed switch wired as Noormally Open (NO).
+# Reference design uses a Reed switch wired as Normally Open (NO).
 from gpiozero import Button
 from signal import pause
 import threading
@@ -9,7 +9,9 @@ import notifier
 
 reed = Button(24) #TODO: Make this configurable
 
-if __name__ == '__main__':
-	notifier.start_server()
-	threading.Thread(target=notifier.accept_conn).start()
-	reed.when_pressed = notifier.send_to_all(b'Switch closed')
+reed.when_pressed = notifier.send_to_all(b'Switch closed')
+
+notifier.start_server()
+notifier.accept_conn()
+
+pause()
