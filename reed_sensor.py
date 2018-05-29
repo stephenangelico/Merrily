28080d124b3051a0ae5d6d64d1fe858eba4d7874
Stephen Angelico <raaff35lightning@gmail.com>
2018-05-29 22:17:13 +1000

    reed_sensor: Pull out testring argument from doorbell.py

--- reed_sensor.py
+++ reed_sensor.py
@@ -10,9 +10,6 @@ import notifier
 reed = Button(24) #TODO: Make this configurable
 
 if __name__ == '__main__':
-	if len(sys.argv) > 1 and sys.argv[1] == 'testring':
-		test_ring()
-	else:
-		notifier.start_server()
-		threading.Thread(target=notifier.accept_conn).start()
-		reed.when_pressed = notifier.send_to_all(b'Switch closed')
+	notifier.start_server()
+	threading.Thread(target=notifier.accept_conn).start()
+	reed.when_pressed = notifier.send_to_all(b'Switch closed')
