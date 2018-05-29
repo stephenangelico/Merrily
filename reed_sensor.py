5aa396c5dd6618c9149a6f82af12b0d7fa06f72d
Stephen Angelico <raaff35lightning@gmail.com>
2018-05-29 23:32:12 +1000

    reed_sensor: Put notification code into function

--- reed_sensor.py
+++ reed_sensor.py
@@ -9,9 +9,12 @@ import notifier
 
 reed = Button(24) #TODO: Make this configurable
 
-reed.when_pressed = notifier.send_to_all(b'Switch closed')
+def test_switch():
+	print("Switch closed")
+	notifier.send_to_all(b'Switch closed')
+	print("Notification sent")
+
+reed.when_pressed = test_switch
 
 notifier.start_server()
 notifier.accept_conn()
-
-pause()
