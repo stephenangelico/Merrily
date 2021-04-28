6f06a7c517ac467599577570a94c0c3e97c55bcb
Stephen Angelico <raaff35lightning@gmail.com>
2021-04-28 21:34:25 +1000

    socket_client: Refactor into class-based connection, making heartbeat work

--- socket_client.py
+++ socket_client.py
@@ -8,79 +8,85 @@ from config import DOORBELL_SERVER, DOORBELL_PORT, NOTIFY_COMMAND, PLAYER_COMMAN
 
 host = 'localhost'
 port = 8090
-latest_ring = {"id": 0, "timestamp": 0.0, "source": ''}
-heartbeat = 0
 
-def start_client():
-	global sock
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.connect((host,port))
-	print("Connected to %s:%s" % (host, port), file=sys.stderr, flush=True)
+class Conn(object):
+	latest_ring = {"id": 0, "timestamp": 0.0, "source": ''}
+	heartbeat = True
 
-def read_socket():
-	buffer = b""
-	while True:
-		data = sock.recv(1024)
-		if not data:
-			break
-		buffer += data
-		while b"\n" in buffer:
-			line, buffer = buffer.split(b"\n", 1)
-			line = line.rstrip().decode("utf-8")
-			if ":" in line:
-				attr, value = line.split(":", 1)
-				value = value.strip()
-				if attr == "Latest Ring":
-					id, timestamp, source = value.split()
-					try:
-						id = int(id)
-						timestamp = float(timestamp)
-						source = str(source)
-					except ValueError:
-						pass # Do not overwrite latest_ring
-					else:
-						global latest_ring
-						latest_ring["id"] = id
-						latest_ring["timestamp"] = timestamp
-						latest_ring["source"] = source
-				elif attr == "New Ring":
-					id, timestamp, source = value.split()
-					try:
-						id = int(id)
-						timestamp = float(timestamp)
-						source = str(source)
-					except ValueError:
-						pass # Do not ring
-					else:
-						if id == latest_ring["id"]:
-							pass # Duplicate ring event
-							# If server resets and client somehow doesn't immediately, it should either timeout
-							# or get BrokenPipeError and reset.
-						elif id > latest_ring["id"]:
-							ring() # Clearly a new ring
-						elif timestamp > (latest_ring["timestamp"] + 5):
-							ring() # New ring after server reset
-				elif attr == "Heartbeat":
-					global heartbeat
-					heartbeat = time.time()
+	def __init__(self):
+		self.connect()
+		threading.Thread(target=self.read_socket, daemon=True).start()
+		self.keepalive()
 
-def ring():
-	subprocess.run(NOTIFY_COMMAND)
-	subprocess.run(PLAYER_COMMAND) # Throw an error if not available
+	def connect(self):
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.connect((host,port))
+		print("Connected to %s:%s" % (host, port), file=sys.stderr, flush=True)
 
-def heartbeat():
-	while True:
-		sock.send(("Heartbeat: Send\r\n").encode("utf-8"))
-		time.sleep(5)
-		if time.time() > (heartbeat + 5): # No response from server after 5 sec
-			pass # TODO: restart the connection
-		else:
-			time.sleep(55)
+	def read_socket(self):
+		buffer = b""
+		while True:
+			data = self.sock.recv(1024)
+			if not data:
+				break
+			buffer += data
+			while b"\n" in buffer:
+				line, buffer = buffer.split(b"\n", 1)
+				line = line.rstrip().decode("utf-8")
+				if ":" in line:
+					attr, value = line.split(":", 1)
+					value = value.strip()
+					if attr == "Latest Ring":
+						id, timestamp, source = value.split()
+						try:
+							id = int(id)
+							timestamp = float(timestamp)
+							source = str(source)
+						except ValueError:
+							pass # Do not overwrite latest_ring
+						else:
+							self.latest_ring["id"] = id
+							self.latest_ring["timestamp"] = timestamp
+							self.latest_ring["source"] = source
+					elif attr == "New Ring":
+						id, timestamp, source = value.split()
+						try:
+							id = int(id)
+							timestamp = float(timestamp)
+							source = str(source)
+						except ValueError:
+							pass # Do not ring
+						else:
+							if id == self.latest_ring["id"]:
+								pass # Duplicate ring event
+								# If server resets and client somehow doesn't immediately, it should either timeout
+								# or get BrokenPipeError and reset.
+							elif id > self.latest_ring["id"]:
+								self.ring() # Clearly a new ring
+							elif timestamp > (self.latest_ring["timestamp"] + 5):
+								self.ring() # New ring after server reset
+					elif attr == "Heartbeat":
+						self.heartbeat = True
+						print("Heartbeat")
+
+	def ring(self):
+		subprocess.run(NOTIFY_COMMAND)
+		subprocess.run(PLAYER_COMMAND) # Throw an error if not available
+
+	def keepalive(self):
+		while self.heartbeat:
+			self.heartbeat = False
+			self.sock.send(("Heartbeat: Send\r\n").encode("utf-8"))
+			time.sleep(10)
+		self.quit()
+
+	def quit(self):
+		self.sock.close()
+
 
 if __name__ == '__main__':
-	start_client()
 	try:
-		threading.Thread(target=read_socket, args=(,), daemon=True).start()
-		threading.Thread(target=heartbeat, args(,), daemon=True).start()
+		while True:
+			conn = Conn()
 	except KeyboardInterrupt:
-		sock.close()
+		conn.quit()
