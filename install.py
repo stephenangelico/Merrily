789584f3291c89a60b5d1e09488a2eb1492b49df
Stephen Angelico <raaff35lightning@gmail.com>
2021-05-19 22:30:06 +1000

    install: Print POC service file to console

--- install.py
+++ install.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 import os
 import sys
+#import argparse
+#parser = argparse.ArgumentParser()
+#parser.parse_args()
 
 # Static data
 template = """[Unit]
-Description=Merrily Doorbell (Module)
+Description=Merrily Doorbell {module}
 After=network.target
 
 [Service]
-Environment=
-User=(User)
-WorkingDirectory=(directory)
-ExecStart=(Python) -u (scriptname.py)
+Environment={environment}
+User={user}
+WorkingDirectory={directory}
+ExecStart={python} -u {script}
 Restart=always
 RestartSec=5
 
 [Install]
 WantedBy=multi-user.target
 """
-#Environment=VIRTUAL_ENV='`pwd`/env'
 bits = {}
 
 # Figure out what to install
@@ -26,18 +28,23 @@ if len(sys.argv) > 1:
 	if sys.argv[1] == "client":
 		bits["module"] = "Client"
 		bits["script"] = "cyril.py"
-		bits["display"] = True # TODO: set to current display
+		bits["service"] = "cyril.service"
 	elif sys.argv[1] == "server":
 		bits["module"] = "Server"
 		bits["script"] = "blanche.py"
+		bits["service"] = "blanche.service"
 	elif sys.argv[1] == "doorbell":
 		bits["module"] = "Sensor"
 		bits["script"] = "doorbell.py"
+		bits["service"] = "doorbell.service"
 else:
 	print("Please specify 'client', 'server' or 'doorbell'.")
 	sys.exit()
 
 # Check if we can/should install
+#if os.getuid() == 0:
+	
+
 if "SUDO_USER" in os.environ:
 	bits["user"] = os.environ["SUDO_USER"]
 else:
@@ -49,9 +56,13 @@ else:
 # Get directory of this script, which we will assume is in the same directory
 # as all scripts to be installed
 bits["directory"] = os.path.dirname(os.path.realpath(sys.argv[0]))
-if "VIRTUAL_ENV" in os.environ:
-	bits["virtualenv"] = os.environ["VIRTUAL_ENV"]
-# If a Python other than the system Python runs this script, assume that
-# Python is the one the user wants the services to be run with.
+# Use instance of Python used to install
 bits["python"] = sys.executable
 
+bits["environment"] = ""
+for var in ["VIRTUAL_ENV", "DISPLAY"]:
+	if var in os.environ:
+		bits["environment"] += (var + "=" + os.environ[var] + " ")
+
+service_file = template.format_map(bits)
+print(service_file)
